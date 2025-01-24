36bb9bcb0afbda1b42ba51988b616129bde20091
Alexkarageorgos <alex_kar1996@hotmail.com>
2025-01-24 12:15:47 +0200

    temp topiko

--- app/src/main/java/com/example/sphy144_har/Feedback.java
+++ app/src/main/java/com/example/sphy144_har/Feedback.java
@@ -1,38 +1,45 @@
 package com.example.sphy144_har;
 
+import static android.view.View.VISIBLE;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
+import android.widget.*;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Feedback extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_feedback);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText etFirstName = findViewById(R.id.et_first_name);
+        EditText etLastName = findViewById(R.id.et_last_name);
+        EditText etPhone = findViewById(R.id.et_phone);
+        EditText etEmail = findViewById(R.id.et_email);
+        EditText etComments = findViewById(R.id.et_comments);
+        Button feedbackButton = findViewById(R.id.SUBMIT);
+        TextView text_confirmation = findViewById(R.id.text_confirmation);
+        feedbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text_confirmation.setVisibility(VISIBLE);
+
+            }
         });
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        // Μεταφορά στην MainActivity όταν ο χρήστης πατήσει πίσω
         Intent intent = new Intent(Feedback.this, MainActivity.class);
         startActivity(intent);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         finish();
     }
-
-    
-
-
 }
